c2f4735c36fdf0c1cf89a980c6f75d962c2575ac
Sergio Martinez-Losa <sergiomtz.losa@gmail.com>
2021-05-28 08:45:30 +0200

    rolling version script

--- rolling_release_docker_qiskit.py
+++ rolling_release_docker_qiskit.py
@@ -14,6 +14,26 @@ def open_file(file_path):
 
     return contents
 
+def replace_readme_file(versions, file_contents):
+
+    if len(versions) != 10:
+        return file_contents
+
+    file_contents = re.sub(r"(?<=QISKIT ).*(?= release)", versions[0], file_contents)
+    file_contents = re.sub(r"(?<=Qiskit ).*(?= including:)", versions[0], file_contents)
+    file_contents = re.sub(r"(?<=Qiskit Terra ).*", versions[1], file_contents)
+    file_contents = re.sub(r"(?<=Qiskit Aer ).*", versions[2], file_contents)
+    file_contents = re.sub(r"(?<=Qiskit Ignis ).*", versions[3], file_contents)
+    file_contents = re.sub(r"(?<=Qiskit Aqua ).*", versions[4], file_contents)
+    file_contents = re.sub(r"(?<=Qiskit IBMQ Provider ).*", versions[5], file_contents)
+    file_contents = re.sub(r"(?<=Qiskit nature ).*", versions[6], file_contents)
+    file_contents = re.sub(r"(?<=Qiskit Optimization ).*", versions[7], file_contents)
+    file_contents = re.sub(r"(?<=Qiskit Finance ).*", versions[8], file_contents)
+    file_contents = re.sub(r"(?<=Qiskit Machine Learning ).*", versions[9], file_contents)
+    file_contents = re.sub(r"(?<=qiskit:qiskit-).*", versions[0], file_contents)
+
+    return file_contents
+
 def replace_dockerfile_versions(versions, file_contents):
 
     if len(versions) != 10:
@@ -37,7 +57,7 @@ def replace_compose_versions(new_release, file_contents):
     if not new_release:
         return file_contents
 
-    file_contents = re.sub(r"(?<=qiskit-service-).*", new_release, file_contents)
+    file_contents = re.sub(r"(?<=qiskit-service-).*", new_release + ":", file_contents)
     file_contents = re.sub(r"(?<=image: qiskit-).*", new_release, file_contents)
     file_contents = re.sub(r"(?<=qiskit-container-).*", new_release, file_contents)
 
@@ -58,8 +78,8 @@ def get_new_versions():
     optimization_release = re.findall(r"(?<=QISKIT_OPTIMIZATION_RELEASE=).*", versions_replace)[0]
     finance_release = re.findall(r"(?<=QISKIT_FINANCE_RELEASE=).*", versions_replace)[0]
 
-    versions_array = [qiskit_release, terra_release, aer_release,
-                      ignis_release, aqua_release, ibmq_release,
+    versions_array = [qiskit_release, terra_release, aer_release, 
+                      ignis_release, aqua_release, ibmq_release, 
                       nature_release, ml_release, optimization_release, finance_release]
 
     return versions_array
@@ -78,6 +98,14 @@ def replace_version_composer_file(release_versions, file_name):
 
     return new_contens
 
+def update_readme(release_versions, file_name):
+
+    readme_contents = open_file(file_name)
+    new_readme = replace_readme_file(release_versions, readme_contents)
+    new_readme = replace_compose_versions(release_versions[0], new_readme)
+
+    return new_readme
+
 new_versions = get_new_versions()
 
 replace_file = "Dockerfile_prod"
@@ -89,10 +117,14 @@ new_contents_offline = replace_version_docker_file(new_versions, replace_file_of
 replace_compose = "docker-compose.yml"
 new_composer = replace_version_composer_file(new_versions, replace_compose)
 
-#print(new_composer)
+replace_readme = "README.md"
+new_readme = update_readme(new_versions, replace_readme)
+
+#print(new_readme)
 
 write_docker_file(replace_file, new_contents)
 write_docker_file(replace_file_offine, new_contents_offline)
 write_docker_file(replace_compose, new_composer)
+write_docker_file(replace_readme, new_readme)
 
 print("Dockerfile QISKIT release files updated!")
